1cfaea6f24e54365cd4441de01a4c17a365581a8
Dorian Hemerick <dorianhemerick@gmail.com>
2020-03-06 01:19:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function logShout(string){
 function logWhisper(string){
   {console.log('hello')}
 }
+
 function sayHiToGrandma(string){
   if(string.toLowerCase === string) {
     return 'I can\'t hear you!'
